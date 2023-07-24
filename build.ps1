f181940d225b40df9269bd343fa99c6320a95da2
MainEditor <armagon052@gmail.com>
2023-07-24 21:52:33 +0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,17 @@
-$ver = Read-Host "Please enter your version in format YY.MM"
+$ver = Read-Host "Please enter your version in format YY.MM.xx"
+
 flet pack --icon favico.ico --name QuickTest.exe --product-name QuickTest --product-version $ver --file-version $ver --copyright https://stepik.org/course/179843/promo main.py
+
+$path = "$env:USERPROFILE\Desktop\QuickTest.exe"
+
+if (Test-Path $path) {
+    Remove-Item $path
+}
+
+Move-Item $PWD"\dist\QuickTest.exe" "$ENV:USERPROFILE\Desktop"
+
+Remove-Item $PWD"\build" -Recurse
+
+Remove-Item $PWD"\QuickTest.exe.spec"
+
+Remove-Item $PWD"\dist"
