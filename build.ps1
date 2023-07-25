d85f30251f2470b94aea868ec81705df4cca404f
MainEditor <armagon052@gmail.com>
2023-07-25 13:15:12 +0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$ver = Read-Host "Please enter your version in format YY.0M.0x"
+$ver = Read-Host "Please enter your version in format YY.0M"
 
 flet pack --icon favico.ico --name QuickTest.exe --product-name QuickTest --product-version $ver --file-version $ver --copyright https://stepik.org/course/179843/promo main.py
 
