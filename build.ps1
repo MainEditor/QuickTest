19bd2ef51f40ca9805e30050824bbd9884639f25
MainEditor <armagon052@gmail.com>
2023-07-26 20:08:02 +0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,12 @@
 $ver = Read-Host "Please enter your version in format YY.0M.MICRO (CalVer)"
 
+Write-Output "-------------------------------------------Start build-------------------------------------------"
+
 flet pack --icon favico2.ico --name QuickTest.exe --product-name QuickTest --product-version $ver --file-version $ver --copyright "https://stepik.org/course/179843/promo" main.py
+
+Write-Output "-------------------------------------------Build Done--------------------------------------------`n`n"
+
+Write-Output "Checking is .exe file already exist and deleting if it exist"
 
 $path = "$env:USERPROFILE\Desktop\QuickTest.exe"
 
@@ -8,10 +14,16 @@ if (Test-Path $path) {
     Remove-Item $path
 }
 
+Write-Output "Moving new .exe file to \Desktop"
 Move-Item $PWD"\dist\QuickTest.exe" "$ENV:USERPROFILE\Desktop"
 
+Write-Output "Deleting \build folder"
 Remove-Item $PWD"\build" -Recurse
 
+Write-Output "Deleting .spec file"
 Remove-Item $PWD"\QuickTest.exe.spec"
 
+Write-Output "Deleting \dist folder"
 Remove-Item $PWD"\dist"
+
+Write-Output "              DONE!               "
