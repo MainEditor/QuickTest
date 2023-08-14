07b2137bb97ff70bdd38fc82c825b732dc09fef3
MainEditor <armagon052@gmail.com>
2023-08-14 13:23:34 +0500

    modified:   src/build.ps1

--- src/build.ps1
+++ src/build.ps1
@@ -2,6 +2,6 @@ $ver = Read-Host "Please enter your version in format YY.0M.MICRO (CalVer)"
 
 Write-Output "-------------------------------------------Start build-------------------------------------------"
 
-flet pack --icon favico.ico --name QuickTest.exe --product-name QuickTest --product-version $ver --file-version $ver --copyright "https://stepik.org/course/179843/promo" --onedir main.py
+flet pack --icon favico.ico --name QuickTest --product-name QuickTest --product-version $ver --file-version $ver --copyright "https://stepik.org/course/179843/promo" --onedir main.py
 
 Write-Output "-------------------------------------------Build Done--------------------------------------------`n`n"
